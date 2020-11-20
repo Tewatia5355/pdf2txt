cb5d6b2fb05318a7de32ab3b4cafdedd770e5d52
YASH TEWATIA <yashbsr3@gmail.com>
2020-11-20 19:15:51 +0530

    added

---
+++ myapi.py
@@ -0,0 +1,31 @@
+import os
+import re
+import textract
+from flask import Flask, request, abort, jsonify, send_from_directory
+
+
+UPLOAD_DIRECTORY = "/api_uploaded_files"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
+
+api = Flask(__name__)
+
+
+@api.route("/pdf2txt/<filename>", methods=["POST"])
+def post_file(filename):
+    """Upload a file."""
+    if "/" in filename:
+        # Return 400 BAD REQUEST
+        abort(400, "no subdirectories allowed")
+    pdfPath = os.path.join(UPLOAD_DIRECTORY, filename)
+    with open(pdfPath, "wb") as fp:
+        fp.write(request.data)
+    text = textract.process(pdfPath)
+    data = re.split('\s{8,}', text.decode("utf-8"))
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    api.run(debug=True, port=8000)
