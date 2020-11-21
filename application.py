3b7fadc1b11661d516bd64560ffa85195463dac2
YASH TEWATIA <yashbsr3@gmail.com>
2020-11-21 18:09:58 +0530

    super

--- application.py
+++ application.py
@@ -18,18 +18,13 @@ def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
 
 
 plagiarism_results = set()
-
+vectors = None
 
 def check_plagiarism():
-    global s_vectors
-    for student_a, text_vector_a in s_vectors:
-        new_vectors = s_vectors.copy()
-        current_index = new_vectors.index((student_a, text_vector_a))
-        del new_vectors[current_index]
-        for student_b, text_vector_b in new_vectors:
-            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
-            score = sim_score*100
-            plagiarism_results.add(score)
+    global vectors
+    sim_score = similarity(vectors[0],vectors[1])[0][1]
+    score = sim_score*100
+    plagiarism_results.add(score)
     return plagiarism_results
 
 
@@ -45,7 +40,7 @@ def normal():
 @application.route("/plag/", methods=["POST"])
 def plag_check():
     """Upload a file."""
-    global s_vectors
+    global vectors
     file = request.form['myarray']
     tf1, tf2 = open("./tmp/t1.txt", "w"), open("./tmp/t2.txt", "w")
     file = ["./tmp/t1.txt", "./tmp/t2.txt"]
@@ -55,7 +50,7 @@ def plag_check():
     tf2.close()
     student_notes = [open(File).read() for File in file]
     vectors = vectorize(student_notes)
-    s_vectors = list(zip(file, vectors))
+    print(vectors)
     ans = list(check_plagiarism())
     return jsonify(ans)
 
@@ -88,5 +83,5 @@ def not_found(e):
 
 
 if __name__ == "__main__":
-    # application.run()
-    application.run(host="0.0.0.0", port=80)
+    application.run()
+    # application.run(host="0.0.0.0", port=80)
