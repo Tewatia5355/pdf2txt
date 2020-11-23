89b6ade2fc1dc59b93ae8f30b24214265b72bf2e
YASH TEWATIA <yashbsr3@gmail.com>
2020-11-23 15:46:50 +0530

    besht

--- application.py
+++ application.py
@@ -17,15 +17,23 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 # def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
 
 
+def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
+def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
+
+
 plagiarism_results = set()
-vectors = None
 
 
 def check_plagiarism():
-    global vectors
-    sim_score = cosine_similarity([vectors[0], vectors[1]])[0][1]
-    score = sim_score*100
-    plagiarism_results.add(score)
+    global s_vectors
+    for student_a, text_vector_a in s_vectors:
+        new_vectors = s_vectors.copy()
+        current_index = new_vectors.index((student_a, text_vector_a))
+        del new_vectors[current_index]
+        for student_b, text_vector_b in new_vectors:
+            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+            score = sim_score*100
+            plagiarism_results.add(score)
     return plagiarism_results
 
 
@@ -41,7 +49,7 @@ def normal():
 @application.route("/plag/", methods=["POST"])
 def plag_check():
     """Upload a file."""
-    global vectors
+   global s_vectors
     file = request.form['myarray']
     tf1, tf2 = open("./tmp/t1.txt", "w"), open("./tmp/t2.txt", "w")
     file = ["./tmp/t1.txt", "./tmp/t2.txt"]
@@ -50,12 +58,10 @@ def plag_check():
     tf1.close()
     tf2.close()
     student_notes = [open(File).read() for File in file]
-    vectors = TfidfVectorizer().fit_transform(student_notes).toarray()
-    sim_score = cosine_similarity([vectors[0], vectors[1]])[0][1]
-    score = sim_score*100
-    os.remove("./tmp/t1.txt")
-    os.remove("./tmp/t2.txt")
-    return jsonify(score)
+    vectors = vectorize(student_notes)
+    s_vectors = list(zip(file, vectors))
+    ans = list(check_plagiarism())
+    return jsonify(ans)
 
 
 @application.route("/pdf2txt/", methods=["POST"])
