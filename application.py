533dcd9449cf8369c20fc47cca38aca023be596e
YASH TEWATIA <yashbsr3@gmail.com>
2020-11-23 15:59:38 +0530

    niocbakjsdbc

--- application.py
+++ application.py
@@ -17,8 +17,8 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 # def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
 
 
-def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
-def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
+# def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
+# def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
 
 
 plagiarism_results = set()
@@ -26,13 +26,16 @@ plagiarism_results = set()
 
 def check_plagiarism():
     global s_vectors
+    def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
+    def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
     for student_a, text_vector_a in s_vectors:
-        new_vectors = s_vectors
+        new_vectors = s_vectors.copy()
         current_index = new_vectors.index((student_a, text_vector_a))
         del new_vectors[current_index]
         for student_b, text_vector_b in new_vectors:
             sim_score = similarity(text_vector_a, text_vector_b)[0][1]
-            score = sim_score*100
+            student_pair = sorted((student_a, student_b))
+            score = (student_pair[0], student_pair[1], sim_score)
             plagiarism_results.add(score)
     return plagiarism_results
 
@@ -57,6 +60,14 @@ def plag_check():
     tf2.write(file[1])
     tf1.close()
     tf2.close()
+
+    def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
+    def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
+    student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+    # student_notes =[open(File).read() for File in  student_files]
+
+    # vectors = vectorize(student_notes)
+
     student_notes = [open(File).read() for File in file]
     vectors = vectorize(student_notes)
     s_vectors = list(zip(file, vectors))
