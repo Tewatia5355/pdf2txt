d19fb00d5192a86142cfce53571b8a02ebee43d9
YASH TEWATIA <yashbsr3@gmail.com>
2020-11-29 16:36:07 +0530

    shit mann

--- imp.py
+++ imp.py
@@ -3,9 +3,9 @@ import random
 
 def cvAuth(sim_score):
     if sim_score < 5.5:
-            return random.random()*10 + 20
+        return random.random()*10 + 20
+    else:
+        if sim_score*8 > 100:
+            return random.random()*10+80
         else:
-            if sim_score*8 > 100:
-                return random.random()*10+80
-            else:
-                return sim_score*8
+            return sim_score*8
